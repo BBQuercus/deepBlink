afa1e0520fab5040854ecb4ef96ba491b3ad5d25
zhanyinx <yinxiu.zhan@fmi.ch>
2020-08-20 16:18:59 +0200

    Made resnet more compact

--- deepblink/networks/resnet.py
+++ deepblink/networks/resnet.py
@@ -23,46 +23,26 @@ def resnet(
         cell_size (int): Size of one cell in the grid.
         filters (int): log2 number of filters in the first convolution layers.
         n_convs (int): number of convolution layers in each convolution block.
-        conv_after_res: If True, performs additional convolution block after residual block.
+        conv_after_res: If True, adds additional convolution block after residual block.
     """
-    i = filters  # 2 ** filters represent the number of filters
-
     if not math.log(cell_size, 2).is_integer():
         raise ValueError(f"cell_size must be a power of 2, but is {cell_size}.")
 
     inputs = tf.keras.layers.Input(shape=(None, None, 1))
+    x = inputs
     skip_layers = []
 
-    # Down: halving
-    x = conv_block(inputs=inputs, filters=2 ** (i), n_convs=n_convs, dropout=dropout)
-    x = residual_block(inputs=x, filters=2 ** (i))
-    if conv_after_res:
-        x = conv_block(inputs=x, filters=2 ** (i + 1), n_convs=n_convs, dropout=dropout)
-        x = tf.keras.layers.Dropout(dropout)(x)
-    skip_layers.append(x)
-    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
-
-    # Down: halving
-    x = conv_block(inputs=x, filters=2 ** (i + 1), n_convs=n_convs, dropout=dropout)
-    x = residual_block(inputs=x, filters=2 ** (i + 1))
-    if conv_after_res:
-        x = conv_block(inputs=x, filters=2 ** (i + 1), n_convs=n_convs, dropout=dropout)
-        x = tf.keras.layers.Dropout(dropout)(x)
-    skip_layers.append(x)
-    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
-
-    # Dynamic down sampling: if cell_size is higher than 4, to get the correct network output shape,
-    # we need to further down-sample by math.log(cell_size, 2) - 2 times.
-    # The -2 corresponds to the 2 halvings already done
-    ndown = max(0, math.log(cell_size, 2) - 2)
+    # Dynamic down sampling: minimum 2 down sampling blocks. Depending on cell_size,
+    # add more down sampling blocks to get the correct output shape
+    ndown = max(2, math.log(cell_size, 2))
     for n in range(int(ndown)):
         x = conv_block(
-            inputs=x, filters=2 ** (i + 1 + n), n_convs=n_convs, dropout=dropout
+            inputs=x, filters=2 ** (filters + n), n_convs=n_convs, dropout=dropout
         )
-        x = residual_block(inputs=x, filters=2 ** (i + 1 + n))
+        x = residual_block(inputs=x, filters=2 ** (filters + n))
         if conv_after_res:
             x = conv_block(
-                inputs=x, filters=2 ** (i + 1 + n), n_convs=n_convs, dropout=dropout
+                inputs=x, filters=2 ** (filters + n), n_convs=n_convs, dropout=dropout,
             )
             x = tf.keras.layers.Dropout(dropout)(x)
         skip_layers.append(x)
@@ -74,22 +54,29 @@ def resnet(
     # If cell_size is 2, we need to upsample 1 == int(2 // 2)
     for n in range(int(2 // cell_size)):
         x = conv_block(
-            inputs=x, filters=2 ** (i + 1 - n), n_convs=n_convs, dropout=dropout
+            inputs=x, filters=2 ** (filters + 1 - n), n_convs=n_convs, dropout=dropout
         )
-        x = residual_block(inputs=x, filters=2 ** (i + 1 - n))
+        x = residual_block(inputs=x, filters=2 ** (filters + 1 - n))
         if conv_after_res:
             x = conv_block(
-                inputs=x, filters=2 ** (i + 1 - n), n_convs=n_convs, dropout=dropout
+                inputs=x,
+                filters=2 ** (filters + 1 - n),
+                n_convs=n_convs,
+                dropout=dropout,
             )
             x = tf.keras.layers.Dropout(dropout)(x)
         x = upconv_block(
-            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (i + 1), n_convs=1
+            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=1
         )
 
     # Connected
-    x = conv_block(inputs=x, filters=2 ** (i + 2), n_convs=n_convs, dropout=dropout)
-    x = residual_block(inputs=x, filters=2 ** (i + 2))
-    x = conv_block(inputs=x, filters=2 ** (i + 1), n_convs=n_convs, dropout=dropout)
+    x = conv_block(
+        inputs=x, filters=2 ** (filters + 2), n_convs=n_convs, dropout=dropout
+    )
+    x = residual_block(inputs=x, filters=2 ** (filters + 2))
+    x = conv_block(
+        inputs=x, filters=2 ** (filters + 1), n_convs=n_convs, dropout=dropout
+    )
     x = tf.keras.layers.Dropout(dropout)(x)
 
     # Logit
