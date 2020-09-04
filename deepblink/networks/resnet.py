a73d0c59cb35344810d954bb8ee83c8abb96f45b
zhanyinx <yinxiu.zhan@fmi.ch>
2020-09-04 16:31:41 +0200

    :pencil: Remove conv from resnet

--- deepblink/networks/resnet.py
+++ deepblink/networks/resnet.py
@@ -4,25 +4,19 @@ import math
 
 import tensorflow as tf
 
-from ._networks import conv_block
 from ._networks import residual_block
 from ._networks import upconv_block
 
 
 def resnet(
-    dropout: float = 0.2,
-    cell_size: int = 4,
-    filters: int = 6,
-    n_convs: int = 3,
-    n_extra_down: int = 0,
+    dropout: float = 0.2, cell_size: int = 4, filters: int = 6, n_extra_down: int = 0,
 ) -> tf.keras.models.Model:
     """Residual network with interspersed dropout.
 
     Arguments:
-        dropout: Percentage of dropout after each conv and resnet layer.
+        dropout: Percentage of dropout after resnet block.
         cell_size: Size of one cell in the prediction matrix.
-        filters: Log2 number of filters in the first convolution layers.
-        n_convs: Number of convolution layers in each convolution block.
+        filters: Log2 number of filters in the first residual block layers.
         n_extra_down: extra downsampling followed by same number of up sampling.
     """
     if not math.log(cell_size, 2).is_integer():
@@ -36,10 +30,9 @@ def resnet(
     # add more down sampling blocks to get the correct output shape
     n_down = int(max(2, math.log(cell_size, 2)))
     for n in range(n_down + n_extra_down):
-        x = conv_block(
-            inputs=x, filters=2 ** (filters + n), n_convs=n_convs, dropout=dropout
-        )
         x = residual_block(inputs=x, filters=2 ** (filters + n))
+        if dropout:
+            x = tf.keras.layers.Dropout(dropout)(x)
         skip_layers.append(x)
         x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
 
@@ -49,28 +42,16 @@ def resnet(
     # If cell_size is 2, we need to upsample 1 == int(2 // 2)
     n_up = int(2 // cell_size)
     for n in range(n_up + n_extra_down):
-        x = conv_block(
-            inputs=x, filters=2 ** (filters + 1 - n), n_convs=n_convs, dropout=dropout
-        )
         x = residual_block(inputs=x, filters=2 ** (filters + 1 - n))
+        if dropout:
+            x = tf.keras.layers.Dropout(dropout)(x)
         x = upconv_block(
-            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=1
+            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=2
         )
 
     # Connected
-    x = conv_block(
-        inputs=x,
-        filters=2 ** (filters + n_down - n_up),
-        n_convs=n_convs,
-        dropout=dropout,
-    )
     x = residual_block(inputs=x, filters=2 ** (filters + n_down - n_up))
-    x = conv_block(
-        inputs=x,
-        filters=2 ** (filters + n_down - n_up),
-        n_convs=n_convs,
-        dropout=dropout,
-    )
+    x = residual_block(inputs=x, filters=2 ** (filters + n_down - n_up))
     x = tf.keras.layers.Dropout(dropout)(x)
 
     # Logit
