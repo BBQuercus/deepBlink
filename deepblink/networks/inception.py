4b7432539b77c79a4263f88a37f277dfcf13f6b4
zhanyinx <yinxiu.zhan@fmi.ch>
2020-09-08 13:49:58 +0200

    :sparkles: Add inception combined with simple conv

--- deepblink/networks/inception.py
+++ deepblink/networks/inception.py
@@ -28,11 +28,13 @@ def inception(
     x = inputs
     skip_layers = []
 
+    x = conv_block(inputs=x, filters=2 ** filters, n_convs=2, dropout=dropout)
+
     # Dynamic down sampling: minimum 2 down sampling blocks. Depending on cell_size,
     # add more down sampling blocks to get the correct output shape
     n_down = int(max(2, math.log(cell_size, 2)))
     for n in range(n_down + n_extra_down):
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + n))
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + n - 2))
         if dropout:
             x = tf.keras.layers.Dropout(dropout)(x)
         skip_layers.append(x)
@@ -44,7 +46,7 @@ def inception(
     # If cell_size is 2, we need to upsample 1 == int(2 // 2)
     n_up = int(2 // cell_size)
     for n in range(n_up + n_extra_down):
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n))
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n - 2))
         if dropout:
             x = tf.keras.layers.Dropout(dropout)(x)
         x = upconv_block(
@@ -52,8 +54,8 @@ def inception(
         )
 
     # Connected
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up))
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up))
+    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
+    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
     x = tf.keras.layers.Dropout(dropout)(x)
 
     # Logit
@@ -61,3 +63,64 @@ def inception(
     x = tf.keras.layers.Activation("sigmoid")(x)
 
     return tf.keras.Model(inputs=inputs, outputs=x)
+
+
+def conv_inception(
+    dropout: float = 0.2, cell_size: int = 4, filters: int = 6, n_extra_down: int = 0,
+) -> tf.keras.models.Model:
+    """Inception network with interspersed dropout.
+
+    Arguments:
+        dropout: Percentage of dropout after each inception block.
+        cell_size: Size of one cell in the prediction matrix.
+        filters: Log2 number of filters in the first inception block.
+        n_extra_down: extra downsampling followed by same number of up sampling.
+    """
+    if not math.log(cell_size, 2).is_integer():
+        raise ValueError(f"cell_size must be a power of 2, but is {cell_size}.")
+
+    inputs = tf.keras.layers.Input(shape=(None, None, 1))
+    x = inputs
+    skip_layers = []
+
+    x = conv_block(inputs=x, filters=2 ** filters, n_convs=2, dropout=dropout)
+
+    # Dynamic down sampling: minimum 2 down sampling blocks. Depending on cell_size,
+    # add more down sampling blocks to get the correct output shape
+    n_down = int(max(2, math.log(cell_size, 2)))
+    for n in range(n_down + n_extra_down):
+        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=2, dropout=dropout)
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + n - 2))
+        if dropout:
+            x = tf.keras.layers.Dropout(dropout)(x)
+        skip_layers.append(x)
+        x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
+
+    # Dynamic upsampling: if cell_size is smaller than 4, after the 2 halvings,
+    # we need to upsample int(2 // cell_size) times
+    # If cell_size is 1, we need to upsample 2 == int(2 // cell_size)
+    # If cell_size is 2, we need to upsample 1 == int(2 // 2)
+    n_up = int(2 // cell_size)
+    for n in range(n_up + n_extra_down):
+        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=2, dropout=dropout)
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n - 2))
+        if dropout:
+            x = tf.keras.layers.Dropout(dropout)(x)
+        x = upconv_block(
+            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=1
+        )
+
+    # Connected
+    x = conv_block(
+        inputs=x, filters=2 ** (filters + n_down - n_up), n_convs=2, dropout=dropout
+    )
+    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
+    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
+    x = tf.keras.layers.Dropout(dropout)(x)
+
+    # Logit
+    x = tf.keras.layers.Conv2D(filters=3, kernel_size=1, strides=1)(x)
+    x = tf.keras.layers.Activation("sigmoid")(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
