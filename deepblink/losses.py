f470a50629493a956576cb30bc8034aad2644a5b
BBQuercus <bastian@eichenbergers.ch>
2020-10-26 15:03:47 +0100

    :pencil: Add experimental focal loss

--- deepblink/losses.py
+++ deepblink/losses.py
@@ -148,6 +148,72 @@ def combined_dice_rmse(y_true, y_pred):
     """Loss that combines dice for probability and rmse for coordinates.
 
     The optimal values for dice and rmse are both 0.
-    Bce is considered more important so we weighted rmse with 1/10.
+    Dice is considered more important so we weighted rmse with 1/10.
     """
-    return dice_loss(y_true[..., 0], y_pred[..., 0]) + rmse(y_true, y_pred) / 10
+    return dice_loss(y_true[..., 0], y_pred[..., 0]) + rmse(y_true, y_pred)  # / 10
+
+
+def binary_focal_loss(gamma=2.0, alpha=0.25):
+    """
+    Binary form of focal loss.
+      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
+      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
+    References:
+        https://arxiv.org/pdf/1708.02002.pdf
+    Usage:
+     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+
+    def binary_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred:  A tensor resulting from a sigmoid
+        :return: Output tensor.
+        """
+        # Take the first axes containing the probability maps
+        y_true = y_true[..., 0]
+        y_pred = y_pred[..., 0]
+        y_true = tf.cast(y_true, tf.float32)
+        # Define epsilon so that the back-propagation will not result in NaN for 0 divisor case
+        epsilon = K.epsilon()
+        # Add the epsilon to prediction value
+        # y_pred = y_pred + epsilon
+        # Clip the prediciton value
+        y_pred = K.clip(y_pred, epsilon, 1.0 - epsilon)
+        # Calculate p_t
+        p_t = tf.where(K.equal(y_true, 1), y_pred, 1 - y_pred)
+        # Calculate alpha_t
+        alpha_factor = K.ones_like(y_true) * alpha
+        alpha_t = tf.where(K.equal(y_true, 1), alpha_factor, 1 - alpha_factor)
+        # Calculate cross entropy
+        cross_entropy = -K.log(p_t)
+        weight = alpha_t * K.pow((1 - p_t), gamma)
+        # Calculate focal loss
+        loss = weight * cross_entropy
+        # Sum the losses in mini_batch
+        loss = K.mean(K.sum(loss, axis=1))
+        return loss
+
+    return binary_focal_loss_fixed
+
+
+def combined_focal_rmse(y_true, y_pred):
+    """Loss that combines dice for probability and rmse for coordinates.
+
+    The optimal values for focal and rmse are both 0.
+    Focal is considered more important so we weighted rmse with 1/10.
+    """
+    loss_rmse = rmse(y_true, y_pred)
+
+    alpha = 1.0
+    gamma = 0.5
+    y_true = y_true[..., :1]
+    y_pred = y_pred[..., :1]
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+    loss_focal = -K.mean(alpha * K.pow(1.0 - pt_1, gamma) * K.log(pt_1)) - K.mean(
+        (1 - alpha) * K.pow(pt_0, gamma) * K.log(1.0 - pt_0)
+    )
+
+    return loss_focal + loss_rmse / 10
+
