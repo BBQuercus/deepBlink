d52ca56db01a6a1f0cabe561bd9a67de4d8f8382
BBQuercus <bastian@eichenbergers.ch>
2020-10-26 15:01:36 +0100

    :pencil: Add squeeze and efficient net

--- deepblink/networks/_networks.py
+++ deepblink/networks/_networks.py
@@ -5,11 +5,18 @@ from typing import Tuple
 import tensorflow as tf
 
 # Default options used in all non-logit convolutional layers.
-OPTIONS_CONV = {"kernel_size": 3, "padding": "same", "kernel_initializer": "he_normal"}
+REG = 1e-6
+OPTIONS_CONV = {
+    "kernel_size": 3,
+    "padding": "same",
+    "kernel_initializer": "he_normal",
+    "kernel_regularizer": tf.keras.regularizers.l2(REG),
+    "bias_regularizer": tf.keras.regularizers.l2(REG),
+}
 
 
 def inception_naive_block(
-    inputs: tf.keras.layers.Layer, filters: int
+    inputs: tf.keras.layers.Layer, filters: int, efficient: bool = True
 ) -> tf.keras.layers.Layer:
     """Inception naive block.
 
@@ -18,35 +25,38 @@ def inception_naive_block(
     Args:
         inputs: Input layer.
         filters: Number of convolutional filters applied.
+        efficient: If the 
     """
-    # 1x1 conv
-    conv1 = tf.keras.layers.Conv2D(
-        filters,
-        (1, 1),
-        activation="relu",
-        padding="same",
-        kernel_initializer="he_normal",
-    )(inputs)
-    # 3x3 conv
-    conv3 = tf.keras.layers.Conv2D(
-        filters * 2,
-        (3, 3),
-        activation="relu",
-        padding="same",
-        kernel_initializer="he_normal",
-    )(inputs)
-    # 5x5 conv
-    conv5 = tf.keras.layers.Conv2D(
-        filters * 4,
-        (5, 5),
-        activation="relu",
-        padding="same",
-        kernel_initializer="he_normal",
-    )(inputs)
-    # 3x3 max pooling
-    pool = tf.keras.layers.MaxPooling2D((3, 3), strides=(1, 1), padding="same")(inputs)
+    args = {
+        "activation": tf.nn.leaky_relu,
+        "padding": "same",
+        "kernel_initializer": "he_normal",
+        # "kernel_regularizer": tf.keras.regularizers.l2(REG),
+    }
 
-    # concatenate filters, assumes filters/channels last
+    # 1x1 conv
+    conv1 = tf.keras.layers.Conv2D(filters, (1, 1), **args)(inputs)
+
+    # 3x3 conv
+    if efficient:
+        conv3 = tf.keras.layers.Conv2D(filters, (1, 1), **args)(inputs)
+        conv3 = tf.keras.layers.Conv2D(filters, (3, 3), **args)(conv3)
+    else:
+        conv3 = tf.keras.layers.Conv2D(filters, (3, 3), **args)(inputs)
+
+    # 5x5 conv
+    if efficient:
+        conv5 = tf.keras.layers.Conv2D(filters, (1, 1), **args)(inputs)
+        conv5 = tf.keras.layers.Conv2D(filters // 2, (5, 5), **args)(conv5)
+    else:
+        conv5 = tf.keras.layers.Conv2D(filters // 2, (5, 5), **args)(inputs)
+
+    # 3x3 max pool
+    pool = tf.keras.layers.MaxPooling2D((3, 3), strides=(1, 1), padding="same")(inputs)
+    if efficient:
+        pool = tf.keras.layers.Conv2D(filters // 2, (1, 1), **args)(pool)
+
+    # Concatenate filters
     layer_out = tf.keras.layers.concatenate([conv1, conv3, conv5, pool], axis=-1)
     return layer_out
 
@@ -55,9 +65,7 @@ def conv_block(
     inputs: tf.keras.layers.Layer, filters: int, n_convs: int = 2, dropout: float = 0
 ) -> tf.keras.layers.Layer:
     """Convolutional block with optional dropout layer.
-
     n_convs * (Conv2D -> ReLU -> Optional Dropout).
-
     Args:
         inputs: Input layer.
         filters: Number of convolutional filters applied.
@@ -67,7 +75,7 @@ def conv_block(
     x = inputs
     for _ in range(n_convs):
         x = tf.keras.layers.Conv2D(filters, **OPTIONS_CONV)(x)
-        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.Activation(tf.nn.leaky_relu)(x)
         x = tf.keras.layers.Dropout(dropout)(x)
     return x
 
@@ -86,7 +94,6 @@ def convpool_skip_block(
     inputs: tf.keras.layers.Layer, filters: int, n_convs: int = 2
 ) -> Tuple[tf.keras.layers.Layer, tf.keras.layers.Layer]:
     """Conv_block with skip connection.
-
     Returns:
         skip: Layer to be used as skip connection. Output from conv_block.
         x: Layer to be used in next process. Output from 2D MaxPooling.
@@ -105,9 +112,7 @@ def upconv_block(
     dropout: float = 0,
 ) -> tf.keras.layers.Layer:
     """Upconvolutional block with skip connection concatenation.
-
     Upsampling -> Conv2D -> ReLU -> Concatenation with skip -> Conv_block.
-
     Args:
         inputs: Input layer.
         skip: Skip connection input layer.
@@ -116,9 +121,12 @@ def upconv_block(
         dropout: If > 0, a dropout layer will be added.
     """
     x = inputs
-    x = tf.keras.layers.UpSampling2D()(x)
+    x = tf.keras.layers.Conv2DTranspose(
+        filters=filters, kernel_size=(2, 2), strides=(2, 2), padding="same"
+    )(x)
+    # x = tf.keras.layers.UpSampling2D()(x)
     x = tf.keras.layers.Conv2D(filters=filters, **OPTIONS_CONV)(x)
-    x = tf.keras.layers.Activation("relu")(x)
+    x = tf.keras.layers.Activation(tf.nn.leaky_relu)(x)
     x = tf.keras.layers.Dropout(dropout)(x)
     x = tf.keras.layers.Concatenate()([skip, x])
     x = conv_block(inputs=x, filters=filters, n_convs=n_convs, dropout=dropout)
@@ -130,7 +138,6 @@ def residual_block(
     inputs: tf.keras.layers.Layer, filters: int
 ) -> tf.keras.layers.Layer:
     """Simple residual block with skip connection addition.
-
     Conv2D -> ReLU (skip) -> Conv2D -> ReLU -> Conv2D -> Addition with skip -> ReLU.
     """
     x = tf.keras.layers.Conv2D(filters=filters, **OPTIONS_CONV)(inputs)
@@ -158,3 +165,71 @@ def logit_block(
         x = tf.keras.layers.Activation("softmax")(x)
 
     return x
+
+
+# TODO: add input and type checks
+def squeeze_block(x: tf.keras.layers.Layer, ratio: int = 8):
+    """Squeeze and excitation block.
+    ref: https://arxiv.org/pdf/1709.01507.pdf.
+    Args:
+        x: Input tensor.
+        ratio: Number of output filters.
+    """
+    filters = x.shape[-1]
+
+    x1 = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x1 = tf.keras.layers.Dense(max(filters // ratio, 1))(x1)
+    x1 = tf.keras.layers.Activation(tf.nn.leaky_relu)(x1)
+    x1 = tf.keras.layers.Dense(filters)(x1)
+    x1 = tf.keras.layers.Activation("sigmoid")(x1)
+    x = tf.keras.layers.Multiply()([x1, x])
+    return x
+
+
+def inception_squeeze_block(
+    inputs: tf.keras.layers.Layer, filters: int, ratio: int = 8
+) -> tf.keras.layers.Layer:
+    """Inception naive block.
+    [Conv2d(1,1), Conv2D(3,3), Conv2D(5,5), MaxPooling2D(3,3)] -> output
+    Args:
+        inputs: Input layer.
+        filters: Number of convolutional filters applied.
+    """
+    # 1x1 conv
+    conv1 = tf.keras.layers.Conv2D(
+        filters,
+        (1, 1),
+        activation=tf.nn.leaky_relu,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=tf.keras.regularizers.l2(0.0002),
+    )(inputs)
+    # 3x3 conv
+    conv3 = tf.keras.layers.Conv2D(
+        filters * 2,
+        (3, 3),
+        activation=tf.nn.leaky_relu,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=tf.keras.regularizers.l2(0.0002),
+    )(inputs)
+    # 5x5 conv
+    conv5 = tf.keras.layers.Conv2D(
+        filters * 4,
+        (5, 5),
+        activation=tf.nn.leaky_relu,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=tf.keras.regularizers.l2(0.0002),
+    )(inputs)
+    # 3x3 max pooling
+    pool = tf.keras.layers.MaxPooling2D((3, 3), strides=(1, 1), padding="same")(inputs)
+
+    # squeeze before concat
+    conv1 = squeeze_block(conv1)
+    conv3 = squeeze_block(conv3)
+    conv5 = squeeze_block(conv5)
+    pool = squeeze_block(pool)
+    # concatenate filters, assumes filters/channels last
+    layer_out = tf.keras.layers.concatenate([conv1, conv3, conv5, pool], axis=-1)
+    return layer_out
--- deepblink/networks/inception.py
+++ deepblink/networks/inception.py
@@ -1,118 +1,74 @@
-# pylint: skip-file
 """Inception network architecture."""
 
 import math
 
 import tensorflow as tf
 
+from ._networks import OPTIONS_CONV
 from ._networks import conv_block
 from ._networks import inception_naive_block
 from ._networks import upconv_block
+from ._networks import squeeze_block
+from ._networks import residual_block
+from ._networks import inception_squeeze_block
 
 
 def inception(
-    dropout: float = 0.2, cell_size: int = 4, filters: int = 6, n_extra_down: int = 0,
+    dropout: float = 0.2,
+    cell_size: int = 4,
+    filters: int = 6,
+    n_extra_down: int = 0,
+    spatial: bool = False,
 ) -> tf.keras.models.Model:
-    """Inception network with interspersed dropout.
+    """Inception combined with squeeze network with interspersed dropout.
 
     Arguments:
         dropout: Percentage of dropout after each inception block.
         cell_size: Size of one cell in the prediction matrix.
         filters: Log2 number of filters in the first inception block.
         n_extra_down: extra downsampling followed by same number of up sampling.
+        spatial: If true, spatial dropout will be used instead of standard dropout.
     """
     if not math.log(cell_size, 2).is_integer():
         raise ValueError(f"cell_size must be a power of 2, but is {cell_size}.")
 
-    inputs = tf.keras.layers.Input(shape=(None, None, 1))
+    inputs = tf.keras.layers.Input(shape=(512, 512, 1))
+    # inputs = tf.keras.layers.Input(shape=(None, None, 1))
     x = inputs
     skip_layers = []
 
-    x = conv_block(inputs=x, filters=2 ** filters, n_convs=2, dropout=dropout)
+    # First conv block
+    for _ in range(2):
+        x = tf.keras.layers.Conv2D(2 ** filters, **OPTIONS_CONV)(x)
+        x = tf.keras.layers.Activation(tf.nn.leaky_relu)(x)
+        x = tf.keras.layers.SpatialDropout2D(dropout)(x)
+        # x = tf.keras.layers.Dropout(dropout)(x)
 
-    # Dynamic down sampling: minimum 2 down sampling blocks. Depending on cell_size,
-    # add more down sampling blocks to get the correct output shape
-    n_down = int(max(2, math.log(cell_size, 2)))
-    for n in range(n_down + n_extra_down):
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + n - 2))
-        x = tf.keras.layers.Dropout(dropout)(x)
+    # Encoder
+    for n in range(2 + n_extra_down):
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + n))
+        x = squeeze_block(x=x)
+        x = tf.keras.layers.SpatialDropout2D(dropout)(x)
+        # x = tf.keras.layers.Dropout(dropout)(x)
         skip_layers.append(x)
         x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
 
-    # Dynamic upsampling: if cell_size is smaller than 4, after the 2 halvings,
-    # we need to upsample int(2 // cell_size) times
-    # If cell_size is 1, we need to upsample 2 == int(2 // cell_size)
-    # If cell_size is 2, we need to upsample 1 == int(2 // 2)
-    n_up = int(2 // cell_size)
-    for n in range(n_up + n_extra_down):
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n - 2))
-        x = tf.keras.layers.Dropout(dropout)(x)
-        x = upconv_block(
-            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=1
-        )
+    # Decoder
+    for n, skip in enumerate(reversed(skip_layers)):
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n))
+        x = squeeze_block(x=x)
+        x = upconv_block(inputs=x, skip=skip, filters=2 ** (filters + 1), n_convs=1)
 
-    # Connected
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
-    x = tf.keras.layers.Dropout(dropout)(x)
-
-    # Logit
-    x = tf.keras.layers.Conv2D(filters=3, kernel_size=1, strides=1)(x)
-    x = tf.keras.layers.Activation("sigmoid")(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=x)
-
-
-def conv_inception(
-    dropout: float = 0.2, cell_size: int = 4, filters: int = 6, n_extra_down: int = 0,
-) -> tf.keras.models.Model:
-    """Inception network with interspersed dropout.
-
-    Arguments:
-        dropout: Percentage of dropout after each inception block.
-        cell_size: Size of one cell in the prediction matrix.
-        filters: Log2 number of filters in the first inception block.
-        n_extra_down: extra downsampling followed by same number of up sampling.
-    """
-    if not math.log(cell_size, 2).is_integer():
-        raise ValueError(f"cell_size must be a power of 2, but is {cell_size}.")
-
-    inputs = tf.keras.layers.Input(shape=(None, None, 1))
-    x = inputs
-    skip_layers = []
-
-    x = conv_block(inputs=x, filters=2 ** filters, n_convs=2, dropout=dropout)
-
-    # Dynamic down sampling: minimum 2 down sampling blocks. Depending on cell_size,
-    # add more down sampling blocks to get the correct output shape
-    n_down = int(max(2, math.log(cell_size, 2)))
-    for n in range(n_down + n_extra_down):
-        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=2, dropout=dropout)
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + n - 2))
-        x = tf.keras.layers.Dropout(dropout)(x)
-        skip_layers.append(x)
+    # # Going back down again
+    n_down = int(math.log(cell_size, 2))
+    for n in range(n_down):
+        x = inception_naive_block(inputs=x, filters=2 ** (filters + n))
+        x = squeeze_block(x=x)
         x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
 
-    # Dynamic upsampling: if cell_size is smaller than 4, after the 2 halvings,
-    # we need to upsample int(2 // cell_size) times
-    # If cell_size is 1, we need to upsample 2 == int(2 // cell_size)
-    # If cell_size is 2, we need to upsample 1 == int(2 // 2)
-    n_up = int(2 // cell_size)
-    for n in range(n_up + n_extra_down):
-        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=2, dropout=dropout)
-        x = inception_naive_block(inputs=x, filters=2 ** (filters + 1 - n - 2))
-        x = tf.keras.layers.Dropout(dropout)(x)
-        x = upconv_block(
-            inputs=x, skip=skip_layers[-(n + 1)], filters=2 ** (filters + 1), n_convs=1
-        )
-
     # Connected
-    x = conv_block(
-        inputs=x, filters=2 ** (filters + n_down - n_up), n_convs=2, dropout=dropout
-    )
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
-    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down - n_up - 2))
-    x = tf.keras.layers.Dropout(dropout)(x)
+    x = inception_naive_block(inputs=x, filters=2 ** (filters + n_down))
+    x = squeeze_block(x=x)
 
     # Logit
     x = tf.keras.layers.Conv2D(filters=3, kernel_size=1, strides=1)(x)
