7c401a82dbcf448a3acbb688931e286a16d76238
zhanyinx <yinxiu.zhan@fmi.ch>
2020-11-02 11:18:32 +0100

    :pencil: Add final model

--- deepblink/losses.py
+++ deepblink/losses.py
@@ -139,8 +139,8 @@ def combined_bce_rmse(y_true, y_pred):
     The optimal values for binary crossentropy (bce) and rmse are both 0.
     Bce is considered more important so we weighted rmse with 1/10.
     """
-    return binary_crossentropy(y_true[..., 0], y_pred[..., 0]) + rmse(
-        y_true, y_pred
+    return (
+        binary_crossentropy(y_true[..., 0], y_pred[..., 0]) + rmse(y_true, y_pred) * 2
     )  # / 10
 
 
@@ -150,4 +150,4 @@ def combined_dice_rmse(y_true, y_pred):
     The optimal values for dice and rmse are both 0.
     Dice is considered more important so we weighted rmse with 1/10.
     """
-    return dice_loss(y_true[..., 0], y_pred[..., 0]) + rmse(y_true, y_pred)  # / 10
+    return dice_loss(y_true[..., 0], y_pred[..., 0]) + rmse(y_true, y_pred) * 2
