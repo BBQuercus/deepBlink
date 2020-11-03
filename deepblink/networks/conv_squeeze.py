25cb15aafd651417888ebf2193d6c29906ff1dfc
zhanyinx <yinxiu.zhan@fmi.ch>
2020-11-03 12:11:19 +0100

    :pencil: final adjustments on convnet with squeeze

--- deepblink/networks/conv_squeeze.py
+++ deepblink/networks/conv_squeeze.py
@@ -1,18 +1,20 @@
-"""Residual network architecture."""
+# pylint: skip-file
+
+"""Fully convolutional networks with / without dropout."""
 
 import math
 
 import tensorflow as tf
 
-from ._networks import residual_block
+from ._networks import conv_block
 from ._networks import squeeze_block
 from ._networks import upconv_block
 
 
-def resnet(
+def conv_squeeze(
     dropout: float = 0.2, cell_size: int = 4, filters: int = 5, n_extra_down: int = 0,
 ) -> tf.keras.models.Model:
-    """Residual blocks combined with squeeze network with interspersed dropout.
+    """Convolutions combined with squeeze network with interspersed dropout.
 
     Arguments:
         dropout: Percentage of dropout after each inception block.
@@ -29,7 +31,7 @@ def resnet(
 
     # Encoder
     for n in range(2 + n_extra_down):
-        x = residual_block(inputs=x, filters=2 ** (filters + n))
+        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=3)
         x = squeeze_block(x=x)
 
         x = tf.keras.layers.SpatialDropout2D(dropout)(x)
@@ -40,14 +42,14 @@ def resnet(
 
     # Decoder
     for n, skip in enumerate(reversed(skip_layers)):
-        x = residual_block(inputs=x, filters=2 ** (filters + n))
+        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=3)
         x = squeeze_block(x=x)
         x = upconv_block(inputs=x, skip=skip)
 
     # # Going back down again
     n_down = int(math.log(cell_size, 2))
     for n in range(n_down):
-        x = residual_block(inputs=x, filters=2 ** (filters + n))
+        x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=3)
         x = squeeze_block(x=x)
         x = tf.keras.layers.SpatialDropout2D(dropout)(x)
         x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
@@ -55,7 +57,7 @@ def resnet(
     x = tf.keras.layers.Concatenate()([skip_bottom, x])
 
     # Connected
-    x = residual_block(inputs=x, filters=2 ** (filters + n))
+    x = conv_block(inputs=x, filters=2 ** (filters + n), n_convs=3)
     x = squeeze_block(x=x)
 
     # Logit
