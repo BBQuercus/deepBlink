40a63c73d286b29756c7e1668ad54b932d009703
BBQuercus <bastian@eichenbergers.ch>
2022-04-11 10:09:50 +0200

    :umbrella: Add tests for predict CLI submodule

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,13 +7,16 @@ import os
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from deepblink.cli._config import HandleConfig
 from deepblink.cli._create import HandleCreate
+from deepblink.cli._predict import HandlePredict
 from deepblink.cli._main import arg_parser
 
 
 LOGGER = logging.Logger("None")
+
 
 # TODO cover more in-depth functionality like commands etc.
 def test_parse_args():
@@ -67,3 +70,70 @@ def test_create():
         assert df["c"].min() >= 0
         assert df["r"].max() <= max_size
         assert df["c"].max() <= max_size
+
+
+# TODO test batch prediction
+@pytest.fixture(scope="session")
+def predict_handler():
+    handler = HandlePredict(
+        arg_input=os.path.join(os.path.dirname(__file__), "./data/image_pixel.tif"),
+        arg_model=os.path.join(os.path.dirname(__file__), "./data/model.h5"),
+        arg_output=None,
+        arg_radius=None,
+        arg_shape=None,
+        arg_probability=None,
+        arg_pixel_size=None,
+        logger=LOGGER,
+    )
+    return handler
+
+
+@pytest.fixture(scope="session")
+def filename_predict_output():
+    return os.path.join(os.path.dirname(__file__), "./data/image_pixel.csv")
+
+
+def test_predict_different_name(predict_handler):
+    filename_special = os.path.join(os.getcwd(), "image_pixel.csv")
+    predict_handler.raw_output = os.getcwd()
+    predict_handler()
+    assert os.path.exists(filename_special)
+    os.remove(filename_special)
+    predict_handler.raw_output = None
+
+
+# Test radius and no output -> same name as input
+def test_predict_radius_and_output(predict_handler, filename_predict_output):
+    predict_handler.radius = 1
+    predict_handler()
+    assert os.path.exists(filename_predict_output)
+    with open(filename_predict_output, "r") as f:
+        assert f.readline().strip() == "y [px],x [px],i"
+    os.remove(filename_predict_output)
+    predict_handler.radius = None
+
+
+def test_predict_probability_and_pixel_size(predict_handler, filename_predict_output):
+    predict_handler.pixel_size = 2.3
+    predict_handler.probability = 0.9
+    predict_handler()
+    with open(filename_predict_output, "r") as f:
+        assert f.readline().strip() == "y [µm],x [µm],p"
+    os.remove(filename_predict_output)
+    predict_handler.pixel_size = None
+    predict_handler.probability = None
+
+
+def test_predict_all_options(predict_handler, filename_predict_output):
+    predict_handler.pixel_size = 2.3
+    predict_handler.probability = 0.9
+    predict_handler.radius = 3
+    predict_handler.raw_shape = "(x,y)"
+    predict_handler()
+    with open(filename_predict_output, "r") as f:
+        assert f.readline().strip() == "y [µm],x [µm],p,i"
+    os.remove(filename_predict_output)
+    predict_handler.pixel_size = None
+    predict_handler.probability = None
+    predict_handler.radius = None
+    predict_handler.raw_shape = None
